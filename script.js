0b3abc5b64557d1d6e08d5b3327aee09b6264d8e
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2020-11-24 20:56:11 -0500

    progress bar and timestamp updates as video plays or user clicks through

--- script.js
+++ script.js
@@ -28,12 +28,27 @@ function updatePlayIcon() {
 
 // update progress & timestamp
 function updateProgress() {
-    return true;
+    // console.log(video.currentTime);
+    progress.value = (video.currentTime / video.duration) * 100;
+
+    // get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10){
+        mins = '0' + String(mins);
+    }
+    
+    // get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10){
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // set video progress 
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // stop video
